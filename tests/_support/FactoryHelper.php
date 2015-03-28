923fa7a13a2c515b9c62826053dce7106ba1c327
Suraj Air <suraj@ajency.in>
2015-03-28 14:00:17 +0530

    factory muffin create project

--- tests/_support/FactoryHelper.php
+++ tests/_support/FactoryHelper.php
@@ -17,17 +17,19 @@ class FactoryHelper extends \Codeception\Module {
 
         $this->factory = new \League\FactoryMuffin\Factory;
 
+        $this->factory->define( 'CommonFloor\Project', array(
+            'cf_project_id' => 'randomNumber',
+            'project_title' => 'sentence|3',
+            'city' => 'city',
+            'project_address' => 'address',
+            'sellable_unit_types' => 'randomDigit',
+            'created_by' => 'factory|CommonFloor\User'
+        ) );
+        
         $this->factory->define( 'CommonFloor\User', array(
             'name' => 'name',
             'email' => 'unique:email',
             'password' => 'password'
-        ) );
-
-        $this->factory->define( 'CommonFloor\Project', array(
-            'cf_project_id' => 'integer',
-            'project_title' => 'sentence|3',
-            'created_by' => 'factory|CommonFloor\User',
-            'updated_by' => 'factory|CommonFloor\User'
         ) );
     }
 
