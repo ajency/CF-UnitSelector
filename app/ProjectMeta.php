be7e8efed7a79f5b9b6dc52dc80d123da8795843
Suraj Air <suraj@ajency.in>
2015-04-01 17:44:28 +0530

    common floor stored values

--- app/ProjectMeta.php
+++ app/ProjectMeta.php
@@ -7,9 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class ProjectMeta extends Model {
 
     protected $table = 'project_meta';
-    
     protected $fillable = ['meta_key', 'meta_value', 'project_id'];
-    
     public $timestamps = false;
+
+    public function toArray() {
+        $data = parent::toArray();
+        if ($data['meta_key'] === 'cf')
+            $data['meta_value'] = unserialize( $data['meta_value'] );
+        $data;
+    }
 
 }
