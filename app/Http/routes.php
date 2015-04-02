08213c508f6f79e1a62b60abb88fe62637ae1aa0
Suraj Air <suraj@ajency.in>
2015-04-02 16:21:38 +0530

    front end html template added

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,7 +18,7 @@ Route::controllers( [
     'password' => 'Auth\PasswordController',
 ] );
 
-Route::get( '/project/{id}', 'ProjectController@show' )->where( 'id', '[0-9]+' );
+Route::get( 'project/{id}', 'ProjectController@show' )->where( 'id', '[0-9]+' );
 
 Route::group( ['prefix' => 'admin', 'middleware' => ['auth']], function() {
     Route::get( '/', 'Admin\AdminController@index' );
@@ -38,3 +38,4 @@ function get_property_type( $type_id ) {
 
     return $types[$type_id];
 }
+
