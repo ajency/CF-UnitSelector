3a8a67a5adb0b5730b1675d65cbb162b8f8bc30d
Suraj Air <suraj@ajency.in>
2015-04-03 13:25:35 +0530

    attributable added

--- database/migrations/2015_04_03_073952_create_room_types_table.php
+++ database/migrations/2015_04_03_073952_create_room_types_table.php
@@ -5,28 +5,25 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateRoomTypesTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('room_types', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->timestamps();
-		});
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create( 'room_types', function(Blueprint $table) {
+            $table->increments( 'id' );
+            $table->string( 'name' );
+        } );
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('room_types');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::drop( 'room_types' );
+    }
 
 }
--- database/migrations/2015_04_03_074342_create_attributes_table.php
+++ database/migrations/2015_04_03_074342_create_attributes_table.php
@@ -5,28 +5,31 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateAttributesTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('attributes', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->timestamps();
-		});
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create( 'attributes', function(Blueprint $table) {
+            $table->increments( 'id' );
+            $table->string( 'label' );
+            $table->enum( ['textbox', 'select', 'radio', 'checkbox', 'textarea', 'media'] );
+            $table->string( 'attributable_type' );
+            $table->integer( 'attributable_id' );
+            $table->text( 'defaults' );
+            $table->timestamps();
+            $table->softDeletes();
+        } );
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('attributes');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::drop( 'attributes' );
+    }
 
 }
