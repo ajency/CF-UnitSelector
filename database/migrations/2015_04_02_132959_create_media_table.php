4c72ad5eb292d1dd02fbcac8a3b72a53b324d2b7
Suraj Air <suraj@ajency.in>
2015-04-07 11:01:46 +0530

    room type table fixed and formatting

--- database/migrations/2015_04_02_132959_create_media_table.php
+++ database/migrations/2015_04_02_132959_create_media_table.php
@@ -5,31 +5,29 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateMediaTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('media', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->string('image_name', 100);
-                        $table->integer('mediable_id')->unsigned();
-                        $table->string('mediable_type', 100);
-                        $table->timestamps();
-		});
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create( 'media', function(Blueprint $table) {
+            $table->increments( 'id' );
+            $table->string( 'image_name', 100 );
+            $table->integer( 'mediable_id' )->unsigned();
+            $table->string( 'mediable_type', 100 );
+            $table->timestamps();
+            $table->softDeletes();
+        } );
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('media');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::drop( 'media' );
+    }
 
 }
