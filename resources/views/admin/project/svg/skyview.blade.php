8d4cef5d5b69ce7165d6bf21b5facdc69763df2b
Suraj Air <suraj@ajency.in>
2015-04-09 15:39:45 +0530

    svg uploads fixed

--- resources/views/admin/project/svg/skyview.blade.php
+++ resources/views/admin/project/svg/skyview.blade.php
@@ -11,16 +11,16 @@
         <input id="skyview_pickfiles" type="button" name="fileToUpload" class="btn btn-small" value="Select your file" data-filename-placement="inside"/>
         <button id="skyview_uploadfiles" type="button" class="btn btn-small btn-primary" data-toggle="collapse" data-target="#proj-master">Upload</button>
         </div>
-        <div id="proj-master" class="svg-holder collapse">
+        <div id="proj-master" class="svg-holder">
             <hr>
-            <div class="svg-tools m-b-20">
+            <div class="svg-tools m-b-20 hidden">
                 <button type="button" class="btn btn-mini btn-default"><i class="fa fa-map-marker"></i> Add Marker</button>
                 <button type="button" class="btn btn-mini btn-default"><i class="fa fa-crosshairs"></i> Mark Area</button>
             </div>
             <div id="skyview_image">  
-            @if(isset($svgImages['skyview']['image_url'][0]))
-            <img src="{{$svgImages['skyview']['image_url'][0]}}" class="img-responsive" >
-            @endif
+                @if(isset($svgImages['skyview']['image_url'][0]))
+                <img width="150" height="150" src="{{$svgImages['skyview']['image_url'][0]}}" class="img-responsive" >
+                @endif
             </div>
             <form>
                 <div class="form-group m-t-10">
