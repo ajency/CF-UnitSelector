3f42b5b8950dc554847a74c39d68559957f0e451
Surekha Rane <surekha@ajency.in>
2015-04-11 17:55:48 +0530

    mouseover classes added

--- public/js/frontend/screen-two/project.view.controller.js
+++ public/js/frontend/screen-two/project.view.controller.js
@@ -147,14 +147,14 @@
 
     CenterBunglowView.prototype.events = {
       'mouseover .layer': function(e) {
-        var html, id, unit, unitType, unitVariant;
+        var availability, html, id, unit, unitType, unitVariant;
         id = e.target.id;
         html = "";
         unit = unitCollection.findWhere({
           id: parseInt(id)
         });
         if (unit === void 0) {
-          html += '<div class="svg-info"> <div class="details"> Villa details not entered </div> </div>';
+          html += '<div class="svg-info"> <div class="details"> Bunglow details not entered </div> </div>';
           $('.layer').tooltipster('content', html);
           return false;
         }
@@ -164,9 +164,12 @@
         unitType = unitTypeCollection.findWhere({
           'id': unit.get('unit_type_id')
         });
+        availability = unit.get('availability');
+        availability = s.decapitalize(availability);
         html = "";
         html += '<div class="svg-info"> <h4 class="pull-left">' + unit.get('unit_name') + '</h4> <span class="label label-success">For Sale</span> <div class="clearfix"></div> <div class="details"> <div> <label>Area</label> - ' + unitVariant.get('super_build_up_area') + ' Sq.ft </div> <div> <label>Unit Type </label> - ' + unitType.get('name') + '</div> </div> </div> </div>';
-        return $('.layer').tooltipster('content', html);
+        $('.layer').tooltipster('content', html);
+        return $('.layer').addClass(availability);
       }
     };
 
