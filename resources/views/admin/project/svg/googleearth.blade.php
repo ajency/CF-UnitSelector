f6c7ce3a4f44a9ab71c12876a33e2f64b7e80092
alisha <alisha@ajency.in>
2015-04-16 11:54:40 +0530

    html changes

--- resources/views/admin/project/svg/googleearth.blade.php
+++ resources/views/admin/project/svg/googleearth.blade.php
@@ -8,31 +8,7 @@
     </div>
     <div class="grid-body">
         <div id="container">
-        <div class="row m-b-10">
-          <div class="col-md-3">
-                  <strong>google-earth-img.jpg</strong>
-                  <button type="button" data-phase-id="1" class="btn btn-mini btn-link remove-phase">
-                  <span class="fa fa-times text-danger"></span></button>
-                   <div class="cf-loader"></div>
-          </div>
-          <div class="col-md-3">
-                  <strong>google-earth-img.jpg</strong>
-                  <button type="button" data-phase-id="1" class="btn btn-mini btn-link remove-phase">
-                  <span class="fa fa-times text-danger"></span></button>
-                   <div class="cf-loader"></div>
-          </div>
-          <div class="col-md-3">
-<strong>google-earth-img.jpg</strong>
-                  <button type="button" data-phase-id="1" class="btn btn-mini btn-link remove-phase">
-                  <span class="fa fa-times text-danger"></span></button>
-                  <div class="cf-loader"></div>
-          </div>
-          <div class="col-md-3">
-               
-                  <img src="http://www.websofia.com/wp-content/uploads/2013/02/gmap-publicity.png" style="width:150px;height:90px;" class="img-thumbnail"/>
-          </div>
-        </div> 
-            <input type="hidden" value="{{ csrf_token()}}" name="_token"/> 
+      <input type="hidden" value="{{ csrf_token()}}" name="_token"/> 
             <input id="pickfiles" type="button" name="fileToUpload" class="btn btn-small" value="Select your file" data-filename-placement="inside"/> 
             <button id="uploadfiles" type="button" class="btn btn-small btn-primary" data-toggle="collapse" data-target="#g-map-1">Upload</button>
         </div>
