6cb6e068f3c4b0f2718d16ac24219ddcf35a77e6
Prajay <prajay@ajency.in>
2015-04-20 11:56:36 +0530

    issue #136

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -654,11 +654,13 @@ function getPositions(floor)
          
             var $el = $("#flat_position");
             $el.empty(); // remove old options
+            $el.append($("<option>Select Position</option>")
+                        .attr("value", '').text(key));
             $.each(newOptions, function (value, key) {
                 $el.append($("<option></option>")
                         .attr("value", value).text(key));
             });
-
+            $(".select-position").removeClass("hidden");
         }
     });
 }
