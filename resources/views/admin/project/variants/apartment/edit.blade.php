59092d51e13fdbc4396689ff3e6f50825561adb2
Prajay <prajay@ajency.in>
2015-04-21 14:34:53 +0530

    issue #143

--- resources/views/admin/project/variants/apartment/edit.blade.php
+++ resources/views/admin/project/variants/apartment/edit.blade.php
@@ -232,10 +232,10 @@
                         <div id="2dlayout_0">
                             @if(isset($layouts[0]['2d']))
                             <img src="{{ $layouts[0]['2d']['IMAGE'] }}" class="img-responsive img-thumbnail">
-                            <button onclick="deleteLayout({{ $layouts[0]['2d']['ID'] }});" type="button" class="btn btn-small btn-default m-t-5 pull-right"><i class="fa fa-trash"></i> Delete</button>
+                            <button onclick="deleteLayout({{ $layouts[0]['2d']['ID'] }}, '2d');" type="button" class="btn btn-small btn-default m-t-5 pull-right"><i class="fa fa-trash"></i> Delete</button>
                             @else
                             <input id="pickfiles_2d_0" type="button" name="fileToUpload" class="btn btn-small btn-white" value="Select your file" data-filename-placement="inside"/>
-                            <button id="uploadfiles_2d_0" type="button" class="btn btn-small btn-primary">Upload</button>												
+                            <button id="uploadfiles_2d_0"  type="button" class="btn btn-small btn-primary">Upload</button>												
                             @endif
                         </div>
                     </div>
@@ -246,7 +246,7 @@
                         <div id="3dlayout_0">
                             @if(isset($layouts[0]['3d']))
                             <img src="{{ $layouts[0]['3d']['IMAGE'] }}" class="img-responsive img-thumbnail">
-                            <button onclick="deleteLayout({{ $layouts[0]['3d']['ID'] }});" type="button" class="btn btn-small btn-default m-t-5 pull-right"><i class="fa fa-trash"></i> Delete</button>
+                            <button onclick="deleteLayout({{ $layouts[0]['3d']['ID'] }}, '3d');" type="button" class="btn btn-small btn-default m-t-5 pull-right"><i class="fa fa-trash"></i> Delete</button>
                             @else
                             <input id="pickfiles_3d_0" type="button" name="fileToUpload" class="btn btn-small btn-white" value="Select your file" data-filename-placement="inside"/>
                             <button  id="uploadfiles_3d_0"type="button" class="btn btn-small btn-primary">Upload</button>												
