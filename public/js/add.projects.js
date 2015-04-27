85381e9ee7fa1538befd13abdcb5aa08e6d1852e
Jeromie Fernandes <jeromie@ajency.in>
2015-04-27 14:13:35 +0530

    tooltip in dashboard fix

--- public/js/add.projects.js
+++ public/js/add.projects.js
@@ -88,7 +88,6 @@
     $('.add-phase-btn').click(function() {
       var phaseName, successFn;
       phaseName = $('.phase-name').val();
-      var objectType = $('div.object-phases').attr('data-object-type'); 
       if (phaseName === '') {
         alert('Please enter phase name');
         return;
@@ -98,19 +97,10 @@
         if (xhr.status === 201) {
           $('.phase-name').val('');
           phaseId = resp.data.phase_id;
-          if(objectType=='building')
-          {
-            phasesContainer = $('select[name="phase_id"]');
-            html = '<option value="{{ phase_id }}">{{ phase_name }}</option>';
-          }
-          else
-          {
-            phasesContainer = $('.phases');
-            html = '<div class="pull-left m-r-10"> <strong>{{ phase_name }}</strong> <button type="button" data-phase-id="{{ phase_id }}" class="btn btn-small btn-link remove-phase"> <span class="fa fa-times text-danger"></span></button> </div>';
-          }
-          
-           compile = Handlebars.compile(html);
-           phasesContainer.append(compile({
+          phasesContainer = $('.phases');
+          html = '<div class="pull-left m-r-10"> <strong>{{ phase_name }}</strong> <button type="button" data-phase-id="{{ phase_id }}" class="btn btn-small btn-link remove-phase"> <span class="fa fa-times text-danger"></span></button> </div>';
+          compile = Handlebars.compile(html);
+          phasesContainer.append(compile({
             phase_name: phaseName,
             phase_id: phaseId
           }));
@@ -247,7 +237,6 @@
       $(this).closest('.row').find('.select-floor').removeClass('hidden');
       floorSelection.empty();
       results = [];
-       results.push(floorSelection.append("<option value=''>Select Floor</option>"));
       for (i = j = 0, ref = noOfFloors; 0 <= ref ? j < ref : j > ref; i = 0 <= ref ? ++j : --j) {
         results.push(floorSelection.append("<option value='" + (i + 1) + "'>" + (i + 1) + "</option>"));
       }
@@ -266,7 +255,7 @@
         success: function(resp) {}
       });
     });
-    return $('.update-response-table').click(function() {
+    $('.update-response-table').click(function() {
       var projectId;
       projectId = $(this).attr('data-p-id');
       return $.ajax({
@@ -275,6 +264,7 @@
         success: function(resp) {}
       });
     });
+    return $('[data-toggle="tooltip"]').tooltip();
   });
 
 }).call(this);
