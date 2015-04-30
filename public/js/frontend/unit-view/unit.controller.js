de6b0c31161f9e6d54c0f537797ffe604a86dc28
Jeromie Fernandes <jeromie@ajency.in>
2015-04-30 18:39:52 +0530

    single image fix in tabs

--- public/js/frontend/unit-view/unit.controller.js
+++ public/js/frontend/unit-view/unit.controller.js
@@ -306,7 +306,7 @@
       response = this.generateLevels();
       html = '';
       $.each(response[0], function(index, value) {
-        return html += '<img class="img" data-src="' + value + '" /><span>' + s.replaceAll(response[2][index], "_", " ") + '</span>';
+        return html += '<div class="layouts animated fadeIn"> <a class="fancybox" href="' + value + '"> <img class="img" data-src="' + value + '" /> <div class="img-overlay"></div> <span>' + s.replaceAll(response[2][index], "_", " ") + '</span> </a> </div>';
       });
       $('.twoD').addClass('current');
       $('.threeD').removeClass('current');
@@ -322,7 +322,7 @@
         $('.gallery').removeClass('current');
       }
       $('.images').html(html);
-      $('.level').attr('class', 'level ' + _.last(response[2]));
+      $('.level').attr('class', 'level Level_0 ' + _.last(response[2]));
       if (response[3].get('external3durl') !== void 0) {
         html = '<img class="img lazy-hidden"  data-src="' + response[3].get('external3durl') + '" />';
         $('.images').html(html);
--- resources/assets/coffee/frontend/unit-view/unit.controller.coffee
+++ resources/assets/coffee/frontend/unit-view/unit.controller.coffee
@@ -381,7 +381,13 @@ class CenterUnitView extends Marionette.ItemView
 		response = @generateLevels()
 		html = ''
 		$.each response[0],(index,value)->
-			html += '<img class="img" data-src="'+value+'" /><span>'+s.replaceAll(response[2][index], "_", " ")+'</span>'
+			html += '<div class="layouts animated fadeIn">
+						<a class="fancybox" href="'+value+'">
+							<img class="img" data-src="'+value+'" />
+							<div class="img-overlay"></div>
+							<span>'+s.replaceAll(response[2][index], "_", " ")+'</span>
+						</a>
+					</div>'
 		$('.twoD').addClass('current')
 		$('.threeD').removeClass('current')
 		$('.external').removeClass('current')
@@ -397,7 +403,7 @@ class CenterUnitView extends Marionette.ItemView
 		
 
 		$('.images').html html
-		$('.level').attr 'class' , 'level '+ _.last(response[2])
+		$('.level').attr 'class' , 'level Level_0 '+ _.last(response[2])
 			
 			
 
