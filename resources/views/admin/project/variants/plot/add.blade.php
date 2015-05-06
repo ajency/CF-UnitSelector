d425ea2340017952bcb332d25f83a75122e697fd
Ajency <alisha@ajency.in>
2015-05-06 10:19:57 +0530

    alignment of textbooks

--- resources/views/admin/project/variants/plot/add.blade.php
+++ resources/views/admin/project/variants/plot/add.blade.php
@@ -36,7 +36,7 @@
                     <div class="col-md-4">
                         <div class="form-group">
                             <label class="form-label">Unit Type</label>
-                            <select name="unit_type" class="select2 form-control" data-parsley-required>
+                            <select name="unit_type" class="select2 form-control m-b-5" data-parsley-required>
                                 <option value="">Select Unit Type</option>
                                 @foreach($unit_type_arr as $unit_type)
                                 <option value="{{$unit_type['id']}}">{{$unit_type['unittype_name']}}</option>
