b7c29c37f1f881d47a1e2b52e1e8db7fc7e24589
Ajency <alisha@ajency.in>
2015-05-16 15:02:53 +0530

    project configuration,SVG and costs pages(new layouts)

--- resources/views/auth/password.blade.php
+++ resources/views/auth/password.blade.php
@@ -1,11 +1,10 @@
 @extends('app')
 
 @section('content')
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
+<div class="container">
+    <div class="panel panel-default">
+             <img src="http://ak.asset2.cfcdn.com/cfassets/images/logo-tab.png" class="inline">
+<span class="header">Reset Password</span>
                 <div class="panel-body">
                     @if (session('status'))
                     <div class="alert alert-success">
@@ -23,28 +22,27 @@
                         </ul>
                     </div>
                     @endif
-
+                    <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="/password/email">
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
                         <div class="form-group">
-                            <label class="col-md-4 control-label">E-Mail Address</label>
-                            <div class="col-md-6">
+                            <label class="form-label">E-Mail Address</label>
+                            <div >
                                 <input type="email" class="form-control" name="email" value="{{ old('email') }}">
                             </div>
                         </div>
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary" style="background:#f68121;color:#fff;border: none;">
+                        <div class="form-group text-center">
+                            <button type="submit" class="btn btn-primary" style="background:#f68121;color:#fff;border: none;">
                                     Send Password Reset Link
                                 </button>
-                            </div>
                         </div>
                     </form>
                 </div>
+                </div>
             </div>
-        </div>
-    </div>
+       
+   
 </div>
 @endsection
