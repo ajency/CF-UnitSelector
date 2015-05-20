c1fbe443f5ec4d6096a7870ba8b237c51897ff2e
Surekha Rane <surekha@ajency.in>
2015-05-20 12:35:51 +0530

    bug fixes

--- public/js/frontend/building-step3/apartments.master.controller.js
+++ public/js/frontend/building-step3/apartments.master.controller.js
@@ -549,6 +549,7 @@
           $('.firstimage').attr('src', transitionImages[0]);
           url = Backbone.history.fragment;
           console.log(building_id = url.split('/')[1]);
+          console.log($('#' + building_id + '.building'));
           return $('#' + building_id + '.building').attr('class', 'layer building active_bldg');
         });
       }
