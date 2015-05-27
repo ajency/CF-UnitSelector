7485dd8c4fb63dc09c6450b5b1fe0194d2cdef7e
Nutan Kamat <nutan@ajency.in>
2015-05-27 14:59:18 +0530

    permissions for rest api methods for svg

--- app/Http/Middleware/Permission.php
+++ app/Http/Middleware/Permission.php
@@ -114,6 +114,9 @@ class Permission {
                               'admin/user/validateuseremail'=>'manage_users', 
                               'admin/user/validateuserpassword'=>'manage_users', 
                               'admin/project/{id}/master/authoring-tool'=>'configure_project',
+                              'admin/project/{project}/svg-tool'=>'configure_project',
+                              'admin/project/{project}/svg-tool/{svg_tool}'=>'configure_project',
+                              'admin/project/{projectid}/image/{imageid}'=>'configure_project',
                 ];
                
                 
