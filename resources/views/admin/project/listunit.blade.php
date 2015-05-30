7ab00925e63b0c1bf91f3c409c24b86072778aae
Prajay <prajay@ajency.in>
2015-05-30 18:29:27 +0530

    Phase

--- resources/views/admin/project/listunit.blade.php
+++ resources/views/admin/project/listunit.blade.php
@@ -32,6 +32,7 @@
                             <th>Name</th>
                             <th>Status</th>
                             <th>Variant</th>
+                            <th>Phase</th>
                             <th>Created On</th>
                             <th>Modified On</th>
                         </tr>
@@ -43,6 +44,7 @@
                                 <td>{{ $unit['unit_name'] }}</td>
                                 <td>{{ ucfirst($unit->availability) }}</td>
                                 <td>{{ $unit->unitVariant->unit_variant_name}}</td>
+                                <td>{{ $unit->phase->phase_name }}</td>
                                 <td>{{ date('d/m/Y',strtotime($unit['created_at'])) }}</td>
                                 <td>{{  date('d/m/Y',strtotime($unit['updated_at'])) }}</td>
                             </tr>
