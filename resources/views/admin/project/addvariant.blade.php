2a76d160852d291544ae101ce83ba622257cc39c
Prajay <prajay@ajency.in>
2015-06-01 11:01:57 +0530

    variantadd bug

--- resources/views/admin/project/addvariant.blade.php
+++ resources/views/admin/project/addvariant.blade.php
@@ -218,7 +218,7 @@
                                         <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapse1" aria-expanded="false">
                                         Level 1
                                         <input type="hidden" value="1" name="levels[]">
-                                        <input style="float:right" type="button" value="Delete Level" class="" onclick="deleteLevel({{ $level }});">
+                                        <input style="float:right" type="button" value="Delete Level" class="" onclick="deleteLevel(1);">
                                         </a>
                                     </h4>
                                     </div>
