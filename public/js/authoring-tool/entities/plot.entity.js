1552f93870b03ee7702105c4dac18bccf62fad88
Nutan Kamat <nutan@ajency.in>
2015-06-04 18:57:41 +0530

    plot fix

--- public/js/authoring-tool/entities/plot.entity.js
+++ public/js/authoring-tool/entities/plot.entity.js
@@ -9,7 +9,7 @@
       return PlotView.__super__.constructor.apply(this, arguments);
     }
 
-    PlotView.prototype.template = Handlebars.compile('<form id="add-form"><div class="form-group"> <label class="unitName">Unit :</label><span class="unit_name"></span> <label class="unit-label" for="exampleInputPassword1">Units</label> <select class="form-control units"> <option value="">Select</option> {{#options}} <option value="{{id}}">{{name}}</option> {{/options}} </select> </div> <div class="checkbox"> <label> <input type="checkbox" name="check_primary"> Mark as primary unit </label> </div> </form>');
+    PlotView.prototype.template = Handlebars.compile('<form id="add-form"><div class="form-group"> <label class="unit-label" for="exampleInputPassword1">Units</label> <select class="form-control units"> <option value="">Select</option> {{#options}} <option value="{{id}}">{{name}}</option> {{/options}} </select> </div> <div class="checkbox"> <label> <input type="checkbox" name="check_primary"> Mark as primary unit </label> </div> </form>');
 
     PlotView.prototype.ui = {
       units: '.units'
