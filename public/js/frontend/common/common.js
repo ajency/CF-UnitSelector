559bb0a681acbb924145f5e1ced81232419b5c27
Surekha Rane <surekha@ajency.in>
2015-06-05 10:22:36 +0530

    issues

--- public/js/frontend/common/common.js
+++ public/js/frontend/common/common.js
@@ -367,7 +367,7 @@
         types = CommonFloor.defaults['type'].split(',');
       }
       id = parseInt(item.id);
-      if ($.inArray(id, filterbuildings) > -1 && notSelectebuildings.length !== actualbuildings.length) {
+      if ($.inArray(id, filterbuildings) > -1 && filterbuildings.length !== actualbuildings.length) {
         return setTimeout(function() {
           return $('#' + id).attr('style', ' stroke-width: 3px; stroke-dasharray: 320 0;stroke-dashoffset: 0; stroke:#F68121; transform: rotateY(0deg) scale(1);-webkit-transform: rotateY(0deg) scale(1);');
         }, Math.random() * 1000);
