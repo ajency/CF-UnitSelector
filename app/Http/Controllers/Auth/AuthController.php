571e3a0efd3a0ac3a73c6a69e56dab235535b95d
Prajay <prajay@ajency.in>
2015-06-10 14:11:11 +0530

    user login status auth

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -3,9 +3,11 @@
 namespace CommonFloor\Http\Controllers\Auth;
 
 use CommonFloor\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use Illuminate\Contracts\Auth\Guard;
 use Illuminate\Contracts\Auth\Registrar;
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
+use Auth;
 
 class AuthController extends Controller {
     /*
@@ -33,6 +35,28 @@ use AuthenticatesAndRegistersUsers;
         $this->registrar = $registrar;
 
         $this->middleware( 'guest', ['except' => 'getLogout'] );
+    }
+    
+    //CUSTOMIZED LOGIN METHOD
+    /*
+    *****Things added to file ***
+    postLogin
+    Illuminate\Http\Request
+    Auth
+
+    */
+    public function postLogin(Request $request)
+    {
+        $email = $request->input('email');
+        $password = $request->input('password');
+        if (Auth::attempt(['email' => $email, 'password' => $password, 'status' => 'active']))
+        {
+            return redirect()->intended('admin/project');
+        }
+        
+        return redirect('/auth/login')->withErrors([
+            'email' => 'The credentials you entered did not match our records. Try again?',
+        ]);
     }
 
     public function redirectPath() {
