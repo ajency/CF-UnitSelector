168c960bf837baeb85468c201e33be5fcddf7c04
Nutan Kamat <nutan@ajency.in>
2015-06-12 12:13:31 +0530

    ip addr whitelist

--- app/Http/Middleware/WhitelistIp.php
+++ app/Http/Middleware/WhitelistIp.php
@@ -13,7 +13,7 @@ class WhitelistIp {
 	 */
 	public function handle($request, Closure $next)
 	{	
-		$whitelistIp =['127.0.0.1','172.20.14.251','172.16.121.73'];
+		$whitelistIp =['127.0.0.1','172.20.14.251','172.16.121.73','172.16.121.203'];
 
 		$clientIpAddress = $request->getClientIp();
 
