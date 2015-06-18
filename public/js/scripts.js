8db6f7b73d35f481e704de6180f8cfb2d64225a2
Prajay <prajay@ajency.in>
2015-06-18 13:23:11 +0530

    save room

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1431,3 +1431,23 @@ function deleteLevel(level)
 
     
 }
+
+function saveRoom()
+{
+    var flag = true;
+        
+    $('.attributes_block select').each(function () {  
+        var controlVal = $(this).closest('.row').find('.tags').val();
+        if(($(this).val()=='select' ||  $(this).val()=='multiple') && controlVal=='') 
+        {
+            var attributename = $(this).closest('.row').find('input').val();
+            alert('Please Enter Default Values For Attribute '+attributename);
+            flag = false;
+        }
+        
+     });    
+
+    if (flag)
+        $('form').submit();
+
+}
