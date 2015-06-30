ede2c6cb62233d0afb84621f23dcfba69dc452b8
Prajay <prajay@ajency.in>
2015-06-30 18:11:08 +0530

    forgot password mail

--- resources/views/admin/user/forgotpasswordmail.blade.php
+++ resources/views/admin/user/forgotpasswordmail.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <!-- BEGIN PAGE TITLE -->
-                <center>
+<center>
                     <table align="center" border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" id="bodyTable" >
                         <tr >
                             <td align="center" valign="top" id="bodyCell">
@@ -37,29 +37,20 @@
                                                     <td valign="top" class="bodyContent" mc:edit="body_content" style="color: #505050;font-family: Arial;font-size: 14px;line-height: 150%;padding-top: 20px;padding-right: 20px;padding-bottom: 20px;padding-left: 20px;text-align: left;">
 
 
-                                                       Dear {{ $user['name']}},
+                                                       Hi {{ $user['name']}},
                                                         <br/>
-                                                        <h3>Welcome to CommonFloor Unit Selector!</h3>
+                                                        <h3>Forgot Password!</h3>
 
-                                                            Your Account on CommonFloor Unit Selector has been created with the following credentials -
-                                                         <br>
+                                                            You had placed a request that you forgot your password.
+                                                           <br>
                                                             <br>
-                                                            <span>Email:</span> {{ $user['email']}}<br>
-                                                            <span>Your account has been set with a randomly generated password :</span> <span class="bold">{{ $user['password']}}</h6></span>
+                                                            We have generated a new password for you . Please use this password to login to your account.
+                                                             <br>
+                                                            <span>New password  :</span> <span class="bold">{{ $user['password']}}</h6></span>
                                                             <br>
-                                                            <br>
-
-                                                            To go back to the Login page, <a href="{{  url() }}">click here</a>
-
-                                                             or copy paste the link below in your browser to login to your account:<br>
-                                                            <a href="{{  url() }}">{{  url() }}</a>
-
                                                             <br/>
 
-                                                            You can update the password to the one of your choice from the profile page.
-                                                            <br/>
-                                                            <br/>
-                                                            If you need any further assitance, feel free to reach us at 1800 180 180 180 or email us at <a href="#">support@commonfloor.com</a> 
+                                                            You can update the password to the one of your choice by updating on the profile page. 
                                                         <br/>
                                                 <br/>
                                                             Thanks,
