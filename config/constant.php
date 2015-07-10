b0b9769fab893506647f918f9f36f8f9cb741a90
Prajay <prajay@ajency.in>
2015-07-10 12:33:18 +0530

    constant file added

---
+++ config/constant.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    /*Mail Configuration*/
+    
+    'host'          => 'smtp.sendgrid.net',
+    
+    'username'          => 'shradha',
+    
+    'password'          => 'ajency#123',
+    
+    /*
+        List of DNS enrty to whitelist ips
+    */
+    
+    'cf_dns_list' => array('localhost',
+                           'stage.bookingcrm.commonfloor.com',
+                           'bookingcrm.commonfloor.com',
+                           'dev.commonfloor.com',
+                           'stage23.commonfloor.com',
+                           'commonfloor.com'
+                          ),
+
+
+];
