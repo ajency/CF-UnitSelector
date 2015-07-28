16cc928fcbae3b35a3f3ba3abdb8a4b3a9e9f194
Nutan Kamat <nutan@ajency.in>
2015-07-28 13:05:22 +0530

    unit summary property type fix

--- app/Http/Controllers/Rest/UnitController.php
+++ app/Http/Controllers/Rest/UnitController.php
@@ -313,8 +313,11 @@ class UnitController extends ApiGuardController {
             
             $propertyTypeId = $unitType->project_property_type_id;
             $unitTypeName = $unitType->unittype_name;
+
+            $defaultPropertyTypeId = ProjectPropertyType::find($propertyTypeId)->property_type_id;
             
-            $default = Defaults::find($propertyTypeId);
+            $default = Defaults::find($defaultPropertyTypeId);
+        
             $projectTypeName = $default->label;
             
             $default = Defaults::find($unitTypeName);
