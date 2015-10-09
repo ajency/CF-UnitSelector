8977b157dd9c72f6fd9cb2f03c8c67fd3889a8cc
Nutan Kamat <nutan@ajency.in>
2015-10-09 13:09:55 +0530

    svg react component

--- resources/react-app-src/components/project-master/svgcontainer.js
+++ resources/react-app-src/components/project-master/svgcontainer.js
@@ -1,17 +1,25 @@
 var React = require('react');
 var PureRenderMixin = require('react-addons-pure-render-mixin');
+var Isvg = require('react-inlinesvg');
 
 var SvgContainer = React.createClass({
     mixins: [PureRenderMixin],		
 
     render: function(){
-            return (
+    	var BASEURL= "http://commonfloorlocal.com";
+    	var svgUrl= BASEURL+'/images/cf-mobile/project.svg';
+        
+        return (
 
                   <div className="svg-area" >
-                    
+                  <Isvg src={svgUrl}>
+	                  Here's some optional content for browsers that don't support XHR or inline
+	                  SVGs. You can use other React components here too. Here, I'll show you.
+
+                  </Isvg>  
                   </div>
-            );
-        }
+        );
+    }
 });
 
 module.exports = SvgContainer;
