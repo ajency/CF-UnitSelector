9de00a68e43daccf422b1514e267c73a8b3fd162
Amit <amit@ajency.in>
2015-10-12 18:13:02 +0530

    card view updated

--- resources/react-app-src/components/project-master/navbar.js
+++ resources/react-app-src/components/project-master/navbar.js
@@ -12,14 +12,14 @@ var NavBar = React.createClass({
                       <div className="navbar-header">
                         <div className="row">
                            <div className="col-xs-2 p-0">
-                                <i className="i-back i-icon"></i>
+                                <i className="i-marker i-icon"></i>
                             </div>
                             <div className="col-xs-6 p-0">
                                 <h3 className="normal margin-none">{this.props.projectTitle} </h3>
                                 <small>{this.props.unitCount} units in your selection</small>
                             </div>
                             <div className="col-xs-4 p-0">
-                              <ul className="list-inline">
+                              <ul className="list-inline pull-right">
                                 <li> <i className="i-phone i-icon"></i></li>
                                 <li onClick={this.props.showFilterModal}> <i className="i-filter i-icon"></i></li>
                               </ul>
