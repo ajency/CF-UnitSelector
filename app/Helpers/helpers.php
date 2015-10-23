b1c378f598e3aeb15a83d5f5352f26c909be4c34
Prajay Verenkar <prajay@ajency.in>
2015-10-23 17:52:11 +0530

    store booking id in units table and set cron job to update blocked units

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -262,3 +262,10 @@ function isValidVariant($projectId,$variantId)
         abort(404);
  
 }
+
+function updateBlockedUnitsToAvailable()
+{
+    $dateTime = date('Y-m-d H:i:s', strtotime('-15 minutes', date('Y-m-d H:i:s')));
+    $units = \CommonFloor\Unit:::where('availability','blocked')->orderBy('project_title')->where('updated_at','>=',$dateTime)->get()->lists('id');
+    $updateUnits = \CommonFloor\Unit:::whereIn('id',  $units)->update(array('availability' => 'available'));
+}
