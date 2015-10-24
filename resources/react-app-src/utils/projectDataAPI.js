a2e7d0f2c76347038b398b5e9a7456aa5fb13e03
Robiul Hoque <robiul@ajency.in>
2015-10-24 15:53:00 +0530

    resolved conflict

--- resources/react-app-src/utils/projectDataAPI.js
+++ resources/react-app-src/utils/projectDataAPI.js
@@ -12,7 +12,6 @@ var Api = {
   // Load project data from server via actions
   getProjectData: function(projectId) {
 
-<<<<<<< HEAD
 
   	//var url = makeUrl('project/' + projectId + '/project-data');
   	 var url = BASE_URL+"/project-data.json";
