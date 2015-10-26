06c727f39e2757bb5c34a0ae05c888d0b11243c4
Nutan Kamat <nutan@ajency.in>
2015-10-26 16:01:23 +0530

    modals based on purpose

--- resources/react-app-src/components/modal/modal.js
+++ resources/react-app-src/components/modal/modal.js
@@ -1,6 +1,7 @@
 var React = require('react');
 var ModalHeader = require('../modal/modalheader');
 var ModalBody = require('../modal/modalbody');
+var classNames = require('classnames');
 
 var Modal = React.createClass({
 
@@ -9,9 +10,11 @@ var Modal = React.createClass({
 
     var modalPurpose = this.props.modalPurpose;
     var backdrop = false;
+    var isFilterModal = true;
 
     if(modalPurpose==="contactModal"){
       backdrop= true;
+      isFilterModal = false;
     }
 
     var modalSettings = {
@@ -24,8 +27,24 @@ var Modal = React.createClass({
 
   render: function () {
     var modalData = this.props.modalData; 
+
+    var modalPurpose = this.props.modalPurpose;
+    var isFilterModal = true;
+
+    if(modalPurpose==="contactModal"){
+      backdrop= true;
+      isFilterModal = false;
+    }
+
+    var modalClasses = classNames({
+      'modal': true,
+      'fade': true,
+      'modal-full-width': isFilterModal,
+      'contactModal': !isFilterModal 
+    });  
+       
     return (
-		<div className="modal fade contactModal" ref="myModal" tabIndex="-1" role="dialog" aria-labelledby="myModalLabel">
+		<div className={modalClasses} ref="myModal" tabIndex="-1" role="dialog" aria-labelledby="myModalLabel">
     		<div className="modal-dialog" role="document">
         		<div className="modal-content">    	
             		<ModalHeader
