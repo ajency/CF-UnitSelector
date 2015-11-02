e74eae1016bdb0e2869ec5d226102c05fdac5c25
Robiul Hoque <robiul@ajency.in>
2015-11-02 11:14:39 +0530

    active tab class added.

--- resources/react-app-src/components/unit-details/unitdetails.js
+++ resources/react-app-src/components/unit-details/unitdetails.js
@@ -31,11 +31,37 @@ var UnitDetails = React.createClass({
     if(!window.isMobile){
         $(document).on('click', '.click', function (e) {
         var theID = $(this).attr('id');
+        $('.list-unstyled li a.active').removeClass('active');
+        $(this).addClass('active');
         $('html, body').animate({
             scrollTop: $('#' + theID + '_div').offset().top-60
         }, 1000);
         return false;
       });
+
+
+        /*var sections = $('.outerDivs')
+          , nav = $('.tabHeader')
+          , nav_height = nav.outerHeight();
+
+          $(window).on('scroll', function () {
+            var cur_pos = $(this).scrollTop();
+            
+            sections.each(function() {
+              var top = $(this).offset().top - nav_height,
+                  bottom = top + $(this).outerHeight();
+              
+              if (cur_pos >= top && cur_pos <= bottom) {
+                nav.find('a').removeClass('active');
+                sections.removeClass('active');
+                
+                $(this).addClass('active');
+                nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+              }
+            });
+          });*/
+
+
 
       function sticky_relocate() {
           var window_top = $(window).scrollTop();
