3b06146c3cb3714f0ccb66de207bc80765061772
Nutan Kamat <nutan@ajency.in>
2015-11-16 11:48:34 +0530

    toggle sun redirect fixed

--- resources/react-app-src/components/steps/stepone.js
+++ resources/react-app-src/components/steps/stepone.js
@@ -318,6 +318,8 @@ var StepOne = React.createClass({
     },     
 
     toggelSunView: function(evt){
+        
+        evt.preventDefault();
         $clickedDiv = $(evt.currentTarget);
 
         if($clickedDiv.hasClass('sun-highlight')){
