328c0e280f7bf3b1478fae4303851e21f88243f8
Prajay Verenkar <prajay@ajency.in>
2015-11-26 15:04:28 +0530

    add rupee sign

--- public/js/unit.js
+++ public/js/unit.js
@@ -106,10 +106,14 @@ function getPriceSheet(id,is_booked,apartment_name,project_id,url){
                       
                     var component_entry = "";
                     for(component_no in result_json.components){
+
+                        if(result_json.components[component_no].amount == '0')
+                            continue;
+
                         component_entry += "<tr>";
                         component_entry +=  "<td>"+result_json.components[component_no].component_price_type+"</td>"; 
                         component_entry +=  "<td>"+ result_json.components[component_no].component_price_sub_type +"</td>"; 
-                        component_entry +=  "<td class='icon-rupee-icn'> "+convertToINR(result_json.components[component_no].amount)+"</td>";  
+                        component_entry +=  "<td class='icon-rupee-icn'> <i class='fa fa-inr'></i> "+convertToINR(result_json.components[component_no].amount)+"</td>";  
                         component_entry += "</tr>";         
                     };
                     var html ='<button type="button" class="close" data-dismiss="modal" aria-label="Close">\
@@ -134,7 +138,7 @@ function getPriceSheet(id,is_booked,apartment_name,project_id,url){
                                         <tr>\
                                             <th> </th>\
                                             <th>Total Sale Value</th>\
-                                            <th class="icon-rupee-icn" style="font-weight:900;"> '+ convertToINR(result_json.total_sale_value) +'</th>\
+                                            <th class="icon-rupee-icn" style="font-weight:900;"> <i class="fa fa-inr"></i> '+ convertToINR(result_json.total_sale_value) +'</th>\
                                         </tr>\
                                     </tfoot>\
                                 </table>\
@@ -187,11 +191,15 @@ function getPaymentPlan(id,is_booked,apartment_name,project_id,url){
                     }
                     payData.sort(-1);
                     for(i=0;i<payData.length;i++){
+
+                        if(result_json.milestones[payData[i][1]].amount == '0')
+                            continue;
+
                         milestone_entry += "<tr>";
                         milestone_entry +=  "<td>"+payData[i][0]+"</td>"; 
                         milestone_entry +=  "<td>"+ result_json.milestones[payData[i][1]].milestone +"</td>"; 
                         
-                        milestone_entry +=  "<td class='icon-rupee-icn'> "+ convertToINR(result_json.milestones[payData[i][1]].amount)+"</td>";  
+                        milestone_entry +=  "<td class='icon-rupee-icn'> <i class='fa fa-inr'></i> "+ convertToINR(result_json.milestones[payData[i][1]].amount)+"</td>";  
                         milestone_entry += "</tr>"; 
                     }
 
@@ -226,7 +234,7 @@ function getPaymentPlan(id,is_booked,apartment_name,project_id,url){
                                         <tr>\
                                             <th> </th>\
                                             <th>Total Sale Value</th>\
-                                            <th class="icon-rupee-icn" style="font-weight:900;"> '+ convertToINR(result_json.total_sale_value) +'</th>\
+                                            <th class="icon-rupee-icn" style="font-weight:900;"><i class="fa fa-inr"></i> '+ convertToINR(result_json.total_sale_value) +'</th>\
                                         </tr>\
                                     </tfoot>\
                                 </table>\
