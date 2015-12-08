57301fb6ffab5190ef5efffef66e09e6aede377b
Nutan Kamat <nutan@ajency.in>
2015-12-08 11:12:00 +0530

    image component

--- resources/react-app-src/components/common/imagecomponent.js
+++ resources/react-app-src/components/common/imagecomponent.js
@@ -1,36 +1,36 @@
 var classNames = require('classnames');
 
 var Image = React.createClass({
-  getInitialState: function() {
-    return {
-      loaded: false
-    };
-  },
+    getInitialState: function() {
+        return {
+          loaded: false
+        };
+    },
 
-  onImageLoad: function() {
-    if (this.isMounted()) {
-      this.setState({loaded: true});
+    onImageLoad: function() {
+        if (this.isMounted()) {
+          this.setState({loaded: true});
+        }
+    },
+
+    componentDidMount: function() {
+        var imgTag = this.refs.img.getDOMNode();
+        var imgSrc = imgTag.getAttribute('src');
+        // You may want to rename the component if the <Image> definition
+        // overrides window.Image
+        var img = new window.Image();
+        img.onload = this.onImageLoad;
+        img.src = imgSrc;
+    },
+
+    render: function() {
+        var {className, ...props} = this.props;
+        var imgClasses = 'image';
+        if (this.state.loaded) {
+          imgClasses = joinClasses(imgClasses, 'image-loaded');
+        }
+        return (
+          <img ref="img" {...props} className={joinClasses(className, imgClasses)} />
+        );
     }
-  },
-
-  componentDidMount: function() {
-    var imgTag = this.refs.img.getDOMNode();
-    var imgSrc = imgTag.getAttribute('src');
-    // You may want to rename the component if the <Image> definition
-    // overrides window.Image
-    var img = new window.Image();
-    img.onload = this.onImageLoad;
-    img.src = imgSrc;
-  },
-
-  render: function() {
-    var {className, ...props} = this.props;
-    var imgClasses = 'image';
-    if (this.state.loaded) {
-      imgClasses = joinClasses(imgClasses, 'image-loaded');
-    }
-    return (
-      <img ref="img" {...props} className={joinClasses(className, imgClasses)} />
-    );
-  }
 });
